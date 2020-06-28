a6cb7910a9c8f2c4c039529a4188c38a7dc23449
Juan Vidal <juan.vidal@daliaresearch.com>
2020-06-28 12:56:57 +0200

    basic server working and serving "Hello Vault" in localhost:8080, with Hyper and Async

--- vaultrs/src/main.rs
+++ vaultrs/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello Vault")))
+}
+
+#[tokio::main]
+async fn main() {
+    // Socket to listen to
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    // Service to handle connections
+    let service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+
+    // Bind service to the socket and serve
+    let server = Server::bind(&addr).serve(service);
+
+    // we capture all errors
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
