133361fdfbee0a2e7dd22b6932592cbba5276388
Juan Vidal <juan.vidal@daliaresearch.com>
2020-06-28 14:04:22 +0200

    Dynamic routes. Now we can pass a string to GET encrypt/<string> and GET decrypt/<String>
    It works, but I don't like it.
    I could not find yet a way to do pattern matching with tuples and regex,
    and that's why I added that ugly if.
    TODO

--- vaultrs/src/main.rs
+++ vaultrs/src/main.rs
@@ -1,22 +1,35 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
+// Routes Regex
+lazy_static! {
+    static ref ENCRYPT_PATH: Regex = Regex::new("^/encrypt/.*?").unwrap();
+    static ref DECRYPT_PATH: Regex = Regex::new("^/decrypt/.*?").unwrap();
+}
+
 async fn requests_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let mut response = Response::new(Body::empty());
+    let path = req.uri().path();
 
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/encrypt") => {
-            *response.body_mut() = Body::from("Encrypted");
+    if req.method() == &Method::GET {
+        match path {
+            path if ENCRYPT_PATH.is_match(path) => {
+                *response.body_mut() = Body::from("Encrypted");
+            }
+            path if DECRYPT_PATH.is_match(path) => {
+                *response.body_mut() = Body::from("Decrypted");
+            }
+            _ => {
+                *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+            }
         }
-        (&Method::GET, "/decrypt") => {
-            *response.body_mut() = Body::from("Decrypted");
-        }
-        _ => {
-            *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
-        }
-    };
+    } else {
+        *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+    }
 
     Ok(response)
 }
