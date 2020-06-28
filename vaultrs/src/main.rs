6604f872a37e6a42121e9c2d5ad72bcbbbbe48f2
Juan Vidal <juan.vidal@daliaresearch.com>
2020-06-28 13:15:37 +0200

    basic routing scaffolding

--- vaultrs/src/main.rs
+++ vaultrs/src/main.rs
@@ -1,10 +1,24 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello Vault")))
+async fn requests_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/encrypt") => {
+            *response.body_mut() = Body::from("Encrypted");
+        }
+        (&Method::GET, "/decrypt") => {
+            *response.body_mut() = Body::from("Decrypted");
+        }
+        _ => {
+            *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+        }
+    };
+
+    Ok(response)
 }
 
 #[tokio::main]
@@ -13,7 +27,8 @@ async fn main() {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     // Service to handle connections
-    let service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+    let service =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(requests_handler)) });
 
     // Bind service to the socket and serve
     let server = Server::bind(&addr).serve(service);
